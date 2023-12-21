900959148949db9283ed48c70f9cf7941770a31f
Krishantha Jayathilake <krishanthajayathilake@gmail.com>
2023-12-21 23:22:22 +0100

    Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import IconButton from '@mui/joy/IconButton';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import './App.css';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+import Link from '@mui/joy/Link';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+
+function ColorSchemeToggle() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+  return (
+    <IconButton
+      id="toggle-mode"
+      size="lg"
+      variant="soft"
+      color="neutral"
+      onClick={() => {
+        if (mode === 'light') {
+          setMode('dark');
+        } else {
+          setMode('light');
+        }
+      }}
+      sx={{
+        position: 'fixed',
+        zIndex: 999,
+        top: '1rem',
+        right: '1rem',
+        borderRadius: '50%',
+        boxShadow: 'sm',
+      }}
+    >
+      {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+    </IconButton>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CssVarsProvider>
+      <CssBaseline />
+      <ColorSchemeToggle />
+      <Sheet sx={{
+        width: 300,
+        mx: 'auto', // margin left & right
+        my: 4, // margin top & bottom
+        py: 3, // padding top & bottom
+        px: 2, // padding left & right
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        borderRadius: 'sm',
+        boxShadow: 'md',
+      }}>
+        <div>
+          <Typography level="h4" component="h1">
+            Welcome!
+          </Typography>
+          <Typography level="body-sm">Sign in to continue.</Typography>
+        </div>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input
+            // html input attribute
+            name="email"
+            type="email"
+            placeholder="johndoe@email.com"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+        </FormControl>
+        <Button sx={{ mt: 1 /* margin top */ }}>
+          Log in
+        </Button>
+        <Typography
+          endDecorator={<Link href="/sign-up">Sign up</Link>}
+          fontSize="sm"
+          sx={{ alignSelf: 'center' }}
+        >
+          Don't have an account?
+        </Typography>
+      </Sheet>
+    </CssVarsProvider>
+  );
 }
 
 export default App
